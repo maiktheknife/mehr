9dfbe805177bad3b869a694e2bbececcd298462e
maiktheknife <maiktheknife@gmail.com>
2016-07-22 18:08:04 +0200

    remove url prefix

--- django/magazin/urls.py
+++ django/magazin/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^mainapp/', include('mainapp.urls')),
+    url(r'', include('mainapp.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
