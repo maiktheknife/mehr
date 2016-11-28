ab990c869a1a36da31704f6dace648741a2b9a30
maiktheknife <maiktheknife@gmail.com>
2016-11-28 18:15:38 +0100

    add parentheses to ensure method call

--- django/mainapp/utils/chapterutils.py
+++ django/mainapp/utils/chapterutils.py
@@ -1,11 +1,17 @@
+import logging
+
+logger = logging.getLogger('mehr')
+
+
 def get_all_chapters_duration(chapters):
 	return sum(map(lambda x: x.duration, chapters))
 
 
 def get_global_chapter_progress(current_chapter, chapters, current_time):
 	all_chapters_duration = get_all_chapters_duration(chapters)
+	logger.debug('get_global_chapter_progress %s %s' % (current_chapter, current_time))
 
 	if all_chapters_duration != 0:
-		return 100.0 * (current_chapter.start_time + current_time) / all_chapters_duration
+		return 100.0 * (current_chapter.start_time() + current_time) / all_chapters_duration
 	else:
 		return 0
